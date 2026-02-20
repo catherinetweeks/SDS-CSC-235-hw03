ee66a5109fe9f530ef883e33cde1ff840a175c08
Catherine Weeks <catherinetweeks@gmail.com>
2026-02-19 21:31:07 -0500

    part 2 complete

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Data from https://github.com/sharmadhiraj/free-json-datasets?tab=readme-ov-file  
+
+// Sources used: 
+// 1. https://www.youtube.com/watch?v=sTOHoueLVJE
+// 2. https://observablehq.com/@d3/d3-group
+// 3. https://www.d3indepth.com/selections/
+// 4. https://www.youtube.com/watch?v=fw1tQOv2U14
+// 5. https://d3js.org/d3-scale-chromatic/categorical
+// 6. https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+
 // DO NOT MODIFY THE DATA, start your code below 
 let myData = [
     {
@@ -299,15 +308,17 @@ let myData = [
     }
   ]
 
-// Sources used: 1. https://www.youtube.com/watch?v=sTOHoueLVJE, 2. https://observablehq.com/@d3/d3-group, 3. https://www.d3indepth.com/selections/, 4. https://www.youtube.com/watch?v=fw1tQOv2U14 
-// Set up dimesions of chart
 
+
+// Bar plot
+
+// Set up dimesions of charts
 const margin = { top: 70, right: 40, bottom: 60, left: 100}
 const width = 800 - margin.left - margin.right
 const height = 600 - margin.top - margin.bottom
+const radius = height / 2 - 50;  // Make a radius for the pie chart
 
-// Create svg container for the chart
-
+// Create svg container for the chart (called bar to differentiate from pie chart)
 const bar = d3.select("#bar-chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -315,34 +326,29 @@ const bar = d3.select("#bar-chart").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   
 // Process data
-
   // Group languages by family and get the count of languages in each family
 let lang_fam_count = d3.rollup(myData, v => v.length, d => d.family);
-  // convert map to array
-myData = Array.from(lang_fam_count, ([family, lang_fam_count]) => ({family, lang_fam_count}));
-
+myData = Array.from(lang_fam_count, ([family, lang_fam_count]) => ({family, lang_fam_count})); // Converts map to an array
 myData.forEach(d => { d.lang_fam_count = +d.lang_fam_count; });
 
-
-// Sort Data
+// Sort data (so it appears greatest to least on the bar graph)
 myData.sort((a, b) => b.lang_fam_count - a.lang_fam_count);
-// X and Y Scales
 
 const x = d3.scaleLinear()
     .range([0, width])
     .domain([0, d3.max(myData, function(d) {return d.lang_fam_count;})]);
-    // Returns max of data
+    // Returns max of data to use for x axis
 
 const y = d3.scaleBand()
     .range([0, height])
-    .padding(0.1)
+    .padding(0.1) // Padding so the bars aren't touching each other.
     .domain(myData.map(function(d) { return d.family; }))
 
 // Create axes 
 const xAxis = d3.axisBottom(x);
 const yAxis = d3.axisLeft(y);
 
-// Add grid lines
+// Add grid lines behind bars for aesthetic and better viewing
 bar.selectAll("line.vertical-grid")
     .data(x.ticks(5))
     .enter()
@@ -365,13 +371,14 @@ bar.selectAll(".bar")
     .attr("x", 0)
     .attr("width", d => x(d.lang_fam_count))
     .attr("fill", "lightblue")
-    .attr("cursor", "pointer")
+    .attr("cursor", "pointer") // Makes the cursor into a pointer when hovering the bars, signifies to users they can click on them.
 
+// Select bars by clicking and un clicking
 bar.selectAll(".bar")
     .on("click", function() {
         const current = d3.select(this).attr("fill");
         d3.select(this)
-            .attr("fill", current === "darkblue" ? "lightblue" : "darkblue");
+            .attr("fill", current === "darkblue" ? "lightblue" : "darkblue"); // If light blue, clicking makes dark blue. If dark blue, clicking makes light blue.
     });
 
 // Add axes
@@ -384,14 +391,14 @@ bar.append("g")
     .call(yAxis);
 
 // Add titles
-bar.append("text")
+bar.append("text") // X Axis
     .attr("transform", "translate(" + width / 2 + "," + (height + margin.bottom /2) + ")")
     .style("text-anchor", "middle")
     .style("font-family", "sans-serif")
     .attr("dy", "1em")
     .text("Number of Languages");
 
-bar.append("text")
+bar.append("text") // Y Axis
     .attr("transform", "rotate(-90)")
     .attr("x", -height / 2)
     .attr("y", -margin.left + 15)
@@ -399,8 +406,7 @@ bar.append("text")
     .style("font-family", "sans-serif")
     .text("Language Family");
 
-// main title
-  bar.append("text")
+  bar.append("text") // Main title
     .attr("x", width/2 - margin.left*2)
     .attr("y", margin.top - 100)
     .style("font-size", "18px")
@@ -411,13 +417,79 @@ bar.append("text")
 
 
 
-
 // Pie chart
 
-// svg container
-const pie = d3.select("#pie-chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// Make the pie chart
+const piechart = d3.select("#pie-chart")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+  .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
 
+// Make the color scheme (used a color scheme with 12 colors as there are 12 language families)
+const color = d3.scaleOrdinal()
+  .domain(myData.map(d => d.family))
+  .range(d3.schemePaired);
+
+// Convert data into angles for pie chart
+const pie = d3.pie()
+  .value(d => d.lang_fam_count);
+
+// Make the arc/slice shapes
+const arc = d3.arc()
+  .innerRadius(0)
+  .outerRadius(radius);
+
+// Add titles
+piechart.append("text") // Main title
+    .attr("x", 0)
+    .attr("y", -height/2 + 30)
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .style("font-weight", "bold")
+    .text("Proportion of Each Language Family");
+
+// Placeholer text on the bottom 
+const selected = piechart.append("text")
+  .attr("x", -100)
+  .attr("y", height/2 - 20)
+  .text("");
+
+// Make it interactive
+const total_langs = d3.sum(myData, d => d.lang_fam_count); // sums up the total number of languages so we can obtain percentages later
+
+// Display language information when corresponding pie slice is clicked
+piechart.selectAll("path")
+  .data(pie(myData))
+  .enter()
+  .append("path")
+  .attr("d", arc)
+  .attr("fill", d => color(d.data.family))
+  .attr("stroke", "white")
+  .attr("cursor", "pointer")
+  .on("click", function(event, d) {
+      selected.text(d.data.family + ": " + d.data.lang_fam_count + " (" + (d.data.lang_fam_count/total_langs * 100).toFixed(2) + "%)");
+  });
+  
+//Add a legend
+const legend = d3.select("#pie-chart svg")
+  .append("g")
+  .attr("transform", "translate(" + width - 130 + ", 20)");
+
+legend.selectAll("rect")
+  .data(myData)
+  .enter()
+  .append("rect")
+  .attr("y", (d, i) => i * 20)
+  .attr("width", 15)
+  .attr("height", 15)
+  .attr("fill", d => color(d.family));
+
+legend.selectAll("text")
+  .data(myData)
+  .enter()
+  .append("text")
+  .attr("x", 20) // So the text doesn't overlap the colors
+  .attr("y", (d, i) => i * 20 + 12) // Evenly spaced
+  .text(d => d.family);
